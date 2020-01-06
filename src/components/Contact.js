df947b6884ac86c02753c1fa5cfe4d0f88b1ac4d
Tiago José <tiago_jvo@outlook.com>
2020-01-06 12:08:32 -0300

    Added showInformation state

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types'
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
 
 const Contact = ({ contact }) => {
+
+    const [showInformation , setShowInformation] = useState(false);
+
+
     return (
-        <div className='card card-body mb-3'>
-            <h4>{contact.name}</h4>
-            <ul className='list-group'>
-                <li className='list-group-item'>{contact.email}</li>
-                <li className='list-group-item'>{contact.phone}</li>
-            </ul>
+        <div className="card card-body mb-3">
+            <h4 onClick={() => setShowInformation(!showInformation)}>
+                {contact.name} <i className="fas fa-sort-down"></i>
+            </h4>
+            {showInformation && (
+                <ul className="list-group">
+                    <li className="list-group-item">{contact.email}</li>
+                    <li className="list-group-item">{contact.phone}</li>
+                </ul>
+            )}
         </div>
     );
 };
 
 Contact.propTypes = {
-    contact : PropTypes.object.isRequired
-}
+    contact: PropTypes.object.isRequired,
+};
 
 export default Contact;
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -5,19 +5,28 @@ class Contacts extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            contacts: [{id: 0 , name: 'Tiago', email: 'tiago_jvo@outlook.com', phone: '999347805' }],
+            contacts: [
+                { id: 0, name: 'Tiago', email: 'tiago_jvo@outlook.com', phone: '999347805'},
+                { id: 1, name: 'José', email: 'idunno@outlook.com', phone: '99654785'},
+            ],
         };
     }
+
 
     render() {
         const { contacts } = this.state;
 
         return (
-            <div>
+            <React.Fragment>
                 {contacts.map((contact) => {
-                    return <Contact key={contact.id} contact={contact}></Contact>;
+                    return (
+                        <Contact
+                            key={contact.id}
+                            contact={contact}
+                        ></Contact>
+                    );
                 })}
-            </div>
+            </React.Fragment>
         );
     }
 }
